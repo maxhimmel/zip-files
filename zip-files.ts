6a157a345ddefaab5c64f4647a79050655f85797
Max Himmel <himmelmax@gmail.com>
2024-09-28 15:44:16 -0700

    use new IArchiveResolver to simplify archive appending

--- zip-files.ts
+++ zip-files.ts
@@ -13,6 +13,8 @@ import { Readable } from "stream";
 import { finished } from "stream/promises";
 import { dirSync, fetchSync, fileSync } from "./lib/archiverHelpers";
 import { webResponseToNodeStream } from "./lib/webToNodeStream";
+import { IArchiveResolver } from "./lib/archiveResolvers/iarchiveResolver";
+import { ArchiveResolverFactory } from "./lib/archiveResolvers/archiveResolverFactory";
 
 // example inputs:
 /*
@@ -49,8 +51,6 @@ void (async function () {
     });
 
     const outputPath = `${path.join(args["outputDir"], args["filename"])}.zip`;
-    const filePaths = args["_"] as unknown as string[];
-    const { urls, files, directories } = parseFileTypes(filePaths);
 
     const outputZip = fs.createWriteStream(outputPath);
     const archive = Archiver.create("zip", {
@@ -62,13 +62,6 @@ void (async function () {
     outputZip.on('close', function () {
         console.log(archive.pointer() + ' total bytes');
         console.log('archiver has been finalized and the output file descriptor has closed.');
-    });
-
-    // This event is fired when the data source is drained no matter what was the data source.
-    // It is not part of this library but rather from the NodeJS Stream API.
-    // @see: https://nodejs.org/api/stream.html#stream_event_end
-    outputZip.on('end', function () {
-        console.log('Data has been drained');
     });
 
     // good practice to catch warnings (ie stat failures and other non-blocking errors)
@@ -88,46 +81,12 @@ void (async function () {
 
     archive.pipe(outputZip);
 
-    for (const f of files) {
-        console.log(`Adding file: ${f}`);
-        await fileSync(archive, f, { name: path.basename(f) });
-        console.log("Finished adding file");
-    }
-
-    for (const d of directories) {
-        console.log(`Adding directory: ${d}`);
-        await dirSync(archive, d, path.basename(d));
-        console.log("Finished adding directory");
-    }
-
-    for (const u of urls) {
-        console.log(`Fetching URL: ${u}`);
-
-        const url = new URL(u);
-        const name = path.join(url.hostname, url.pathname);
-
-        await fetchSync(archive, url, { name });
-        console.log("Finished fetching URL");
+    const filepaths = args["_"] as unknown as string[];
+    for (const f of filepaths) {
+        const archiveResolver = ArchiveResolverFactory.create(f);
+        await archiveResolver.append(archive);
     }
 
     console.log("Finalizing archive ...");
     await archive.finalize();
 })();
-
-function parseFileTypes(filePaths: string[]) {
-    const urls = [] as string[];
-    const files = [] as string[];
-    const directories = [] as string[];
-
-    for (const filePath of filePaths) {
-        if (fileHelpers.isUrl(filePath)) {
-            urls.push(filePath);
-        } else if (fileHelpers.isFile(filePath)) {
-            files.push(filePath);
-        } else if (fileHelpers.isDirectory(filePath)) {
-            directories.push(filePath);
-        }
-    }
-
-    return { urls, files, directories };
-}
